53c29226f1b90b7094e7e80dfa25daf6cef87316
vkutin12q <thinhnp.22git@vku.udn.vn>
2023-07-12 09:13:22 +0700

    update gpt

--- src/chatgpt/gpt.js
+++ src/chatgpt/gpt.js
@@ -1,15 +1,23 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.REACT_APP_API_KEY || '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',
 });
 const openai = new OpenAIApi(configuration);
 
-const chatCompletion = (prompt) => {
-    return openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "assistant", content: prompt }],
+const chatCompletion = async (prompt) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "Say this is a test",
+        max_tokens: 7,
+        temperature: 0,
+    });
+    console.log(response.data);
+    return await openai.createChatCompletion({
+        model: "text-davinci-003",
+        messages: [{ role: "user", content: prompt }],
     });
 }
-
-module.exports = { chatCompletion };
+console.log(process.env.REACT_APP_API_KEY);
+console.log(chatCompletion("Hello, how are you?").data);
+//module.exports = { chatCompletion };
